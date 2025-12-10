fcc2455f13d54cc278adf034be0e0b93c2d738d7
jh020804 <jh020804@gmail.com>
2025-12-10 11:02:10 +0900

    Fix routes in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,34 @@
+import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+// 페이지 import
 import LoginPage from './pages/LoginPage';
 import SignupPage from './pages/SignupPage';
 import DashboardPage from './pages/DashboardPage';
-import ProjectPage from './pages/ProjectPage';
-import MainLayout from './components/MainLayout';
+import ProjectPage from './pages/ProjectPage'; // (중복 삭제함)
 import ChatPage from './pages/ChatPage';
-import ProjectChatRoom from './pages/ProjectChatRoom'; // 1. ProjectChatRoom 불러오기
-import ProjectPage from './pages/ProjectPage';
+import ProjectChatRoom from './pages/ProjectChatRoom';
+import MainLayout from './components/MainLayout';
 
 function App() {
   return (
     <Router>
       <Routes>
-        {/* 레이아웃이 필요 없는 페이지 */}
+        {/* 1. 로그인/회원가입 (레이아웃 없음) */}
+        {/* 처음 접속 시(/) 로그인 페이지로 이동 */}
+        <Route path="/" element={<LoginPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignupPage />} />
 
-        {/* MainLayout을 사용하는 페이지 (중첩 라우팅) */}
+        {/* 2. 메인 서비스 (MainLayout 사용 - 상단바 포함) */}
         <Route element={<MainLayout />}>
-          <Route path="/" element={<DashboardPage />} />
-          <Route path="/project/:projectId" element={<ProjectPage />} />
+          <Route path="/dashboard" element={<DashboardPage />} />
+          
+          {/* ‼️ [중요] 여기에 's'를 붙여서 /projects 로 맞춰야 에러가 해결됩니다! */}
+          <Route path="/projects/:projectId" element={<ProjectPage />} />
+          
           <Route path="/chat" element={<ChatPage />} /> 
-          <Route path="/chat/:projectId" element={<ProjectChatRoom />} /> {/* 2. 채팅방 경로 추가 */}
+          <Route path="/chat/:projectId" element={<ProjectChatRoom />} />
         </Route>
         
       </Routes>
